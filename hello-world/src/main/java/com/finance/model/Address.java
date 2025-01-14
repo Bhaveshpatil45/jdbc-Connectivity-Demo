f0595fb91199b3459d5859f2190f4e4a9e73dd12
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-14 12:13:31 +0530

    model

---
+++ hello-world/src/main/java/com/finance/model/Address.java
@@ -0,0 +1,54 @@
+package com.finance.model;
+
+public class Address {
+
+    private long id;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String pin;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+}
